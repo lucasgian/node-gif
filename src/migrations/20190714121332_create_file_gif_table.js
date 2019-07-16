168b23446e5cd2f223078d16dc8a086ca18d712c
lucasgian <gianextensao@gmail.com>
2019-07-16 16:40:12 -0400

    #3 [migration] alteração na estrutura da tabela gif_file

--- src/migrations/20190714121332_create_file_gif_table.js
+++ src/migrations/20190714121332_create_file_gif_table.js
@@ -4,12 +4,12 @@ exports.up = function(knex) {
     .createTable('gif_file', function (table) {
        table.increments('id');
        table.string('file_name', 128);
-       table.string('file_tag', 68);
-       table.string('file_link', 128);
-       table.string('file_preview', 255);
-       table.string('file_original_still', 255);
-       table.boolean('file_visibility');
+       table.string('file_tag', 68).notNull().defaultTo('without');
+       table.string('file_link', 128).unique();
+       table.text('file_original_still');
+       table.boolean('file_visibility').notNull().defaultTo(false);;
        table.boolean('file_key', 68).nullable();
+       table.dateTime('dateLimit').notNull();
        table.dateTime('createdAt').notNull();
        table.dateTime('updatedAt').nullable();
     });
