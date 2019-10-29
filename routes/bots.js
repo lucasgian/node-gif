2c87da3b056cc2d82773169e02778018e9c0d1c9
lucasgian <gian.fonseca@deskfy.io>
2019-10-29 01:43:08 -0300

    fixed remove method

--- routes/bots.js
+++ routes/bots.js
@@ -9,11 +9,6 @@ const bot_controller = require('@controllers/botController');
 /** GET busca todas as gifs publicas salvas */
 router.get('/bots', bot_controller.getAll);
 
-/** GET busca uma gif especifica, atraves do seu id (param)
-* @param id, recebe o id da gif.
-*/
-router.get('/bots/:id', bot_controller.get);
-
 /** DELETE delete uma gif especifica, atraves do seu id (param)
 * @param id, recebe o id da gif.
 */
